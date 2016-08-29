7444b81ee369b76ce79a4aeaac9e0671e56e63c2
Alexey Buzdin <alexey.buzdin@ctco.lv>
2016-08-29 13:56:02 +0300

    removed unnecessary thing from gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,35 +1,25 @@
-module.exports = function (grunt) {
+module.exports = function(grunt) {
 
-grunt.initConfig({
-  bump: {
-    options: {
-      files: ['package.json'],
-      updateConfigs: [],
-      commit: true,
-      commitMessage: 'Bumping version to %VERSION%',
-      commitFiles: ['package.json'],
-      createTag: false,
-      tagName: 'v%VERSION%',
-      tagMessage: 'Version %VERSION%',
-      push: true,
-      pushTo: 'origin',
-      gitDescribeOptions: '--tags --always --abbrev=1 --dirty=-d',
-      globalReplace: false,
-      prereleaseName: false,
-      metadata: '',
-      regExp: false
-    }
-  },
-})
-
-    grunt.loadNpmTasks('grunt-bump');
-
-    grunt.registerTask('incrementVersion', ['bump', 'teamcity']);
-
-    grunt.task.registerTask('teamcity', 'A task to set version parameter in TeamCity using service messages', function() {
-        var pkgJson = require('./package.json');
-        var version = pkgJson.version;
-        grunt.log.writeln("##teamcity[setProjectParameter npm_timelog-components='" + version + "']");
-        grunt.log.writeln("##teamcity[buildNumber '" + version + "']");
+    grunt.initConfig({
+        bump: {
+            options: {
+                files: ['package.json'],
+                updateConfigs: [],
+                commit: true,
+                commitMessage: 'bumping version to %VERSION%',
+                commitFiles: ['package.json'],
+                createTag: false,
+                tagName: 'v%VERSION%',
+                tagMessage: 'Version %VERSION%',
+                push: true,
+                pushTo: 'origin',
+                gitDescribeOptions: '--tags --always --abbrev=1 --dirty=-d',
+                globalReplace: false,
+                prereleaseName: false,
+                metadata: '',
+                regExp: false
+            }
+        }
     });
+    grunt.loadNpmTasks('grunt-bump');
 };
