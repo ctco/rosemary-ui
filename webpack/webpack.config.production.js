bba83b2f2e16c8124479cabab03404dade6153ab
Alexey Buzdin <alexey.buzdin@ctco.lv>
2016-09-12 14:47:33 +0300

    improved publishing to gh-pages

---
+++ webpack/webpack.config.production.js
@@ -0,0 +1,46 @@
+var path = require('path');
+var autoprefixer = require('autoprefixer');
+var webpack = require('webpack');
+var base = require('./webpack.config.base');
+var webpackUtils = require('./utils/webpack-utils');
+
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = webpackUtils.merge(base.defaults, {
+    entry: {
+        bundle: [
+            './demo/app'
+        ]
+    },
+    output: {
+        path: path.join(__dirname, './../dist'),
+        filename: '[name].js'
+    },
+    resolve: {
+        root: [
+            path.resolve('demo')
+        ]
+    },
+    module: {
+        preLoaders: [
+            { test: /\.js$/, loaders: ['eslint'], include: [path.resolve('src'), path.resolve('demo')] }
+        ],
+        loaders: [
+            { test: /\.js$/, loaders: ['react-hot', 'babel'], include: [path.resolve('src'), path.resolve('demo')] },
+            { test: /\.css$/, loader: ExtractTextPlugin.extract('style', 'css') },
+            { test: /\.scss$/, loader: ExtractTextPlugin.extract('style', 'css!postcss!sass?outputStyle=compact'), include: [path.resolve('src'), path.resolve('demo')] },
+            { test: /\.png$/, loader: 'file', include: [path.resolve('src'), path.resolve('demo')] },
+            { test: /\.(woff|woff2|eot|ttf|svg)$/, loader: 'url?limit=100000', include: [path.resolve('src'), path.resolve('demo')] }
+        ]
+    },
+    devServer: {
+        historyApiFallback: true
+    },
+    plugins: [
+        new webpack.NoErrorsPlugin(),
+        new HtmlWebpackPlugin({
+            template: 'template.html'
+        })
+    ]
+});
