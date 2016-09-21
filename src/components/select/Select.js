4d3b88e1c1b201f276f1f6c7200560e042667983
aigars <aigars.beinarovichs@ctco.lv>
2016-09-21 14:25:19 +0300

    minor ordering fix

--- src/components/select/Select.js
+++ src/components/select/Select.js
@@ -74,13 +74,14 @@ class Select extends React.Component {
             this.setState({open});
         }
 
+        if (this.props.onPopupStateChange) {
+            this.props.onPopupStateChange(open);
+        }
+
         if (open) {
             this.handlePopupOpening();
         }
 
-        if (this.props.onPopupStateChange) {
-            this.props.onPopupStateChange(open);
-        }
     }
 
     isControlled() {
