5a38e20f388fb3009d038386041fadfe0128ee6d
janis.aizvakars <janis.aizvakars@ctco.lv>
2016-10-05 10:58:24 +0300

    NOT FOR SUBMT Main [DEV]

---
+++ demo/components/eventplanner/nonWorkingDays.js
@@ -0,0 +1,10 @@
+export default [
+    new Date(2015, 2, 31),
+    new Date(2015, 4, 2),
+    new Date(2015, 3, 4),
+    new Date(2015, 3, 5),
+    new Date(2015, 3, 19),
+    new Date(2015, 3, 25),
+    new Date(2015, 3, 26),
+    new Date(2015, 3, 20)
+];
