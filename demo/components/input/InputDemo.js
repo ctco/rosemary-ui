b1ef614769880fad1d2b15a59da37d3e43bf59ce
aigars <aigars.beinarovichs@ctco.lv>
2016-10-11 11:34:17 +0300

    (CIS-8370) - Team Planner, Search employee: Icon missing in multiselect search field

--- demo/components/input/InputDemo.js
+++ demo/components/input/InputDemo.js
@@ -9,14 +9,18 @@ export default class InputDemo extends React.Component {
             <DemoWithSnippet>
                 <div className="icon-input">
                     <Input placeholder="small input" className="text-input--sm"/>
-                    <i className="im icon-search icon--xs" />
+                    <i className="im icon-search icon--xs"/>
                 </div>
                 <br/><br/>
                 <div className="icon-input">
                     <Input placeholder="default input" onChange={(value) => console.log(value)} type="text"/>
                     <i className="im icon-search icon--xs"/>
                 </div>
-
+                <br/><br/>
+                <div className="icon-input icon-input--left">
+                    <Input placeholder="default input" onChange={(value) => console.log(value)} type="text"/>
+                    <i className="im icon-search icon--xs"/>
+                </div>
                 <br/><br/>
                 <div className="icon-input">
                     <Input placeholder="large Input" className="text-input--lg"/>
