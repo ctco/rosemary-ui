18e9b8cc911887d11270cef23394045b4a15d525
janis.aizvakars <janis.aizvakars@ctco.lv>
2017-10-13 11:12:52 +0300

    Update Switch Cotnrolled to use value

--- src/components/switch/Switch.stories.js
+++ src/components/switch/Switch.stories.js
@@ -1,13 +1,17 @@
 import React from 'react';
-import {storiesOf, action} from '@kadira/storybook';
-import Switch from './Switch.js';
+import { storiesOf } from '@kadira/storybook';
+import Switch from './Switch';
+
 
 storiesOf('Switch', module)
-    .add('Checked', () => (
-        <Switch onChange={(value) => console.log(value)} checked title="This is Title"/>
+    .add('Controlled', () => (
+        <Switch onChange={(value) => console.log(value)} value={false} title="This is Title"/>
     ))
+    .add('Checked Not Controlled', () => (
+    <Switch onChange={(value) => console.log(value)} checked title="This is Title"/>
+))
     .add('Disabled', () => (
-        <Switch title="title title" checked disabled/>
+        <Switch title="title title" disabled/>
     ))
     .add('Uncontrolled Switch', () => (
         <Switch />
