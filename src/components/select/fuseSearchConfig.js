daa416b5b79ebb9793187bd7d9dba6830b827789
janis.aizvakars <janis.aizvakars@ctco.lv>
2017-10-17 13:26:02 +0300

    Modify fuzzy search threshold

--- src/components/select/fuseSearchConfig.js
+++ src/components/select/fuseSearchConfig.js
@@ -1,10 +1,11 @@
 export default {
+    tokenize: true,
     shouldSort: true,
-    threshold: 0.5,
-    location: 0,
-    distance: 100,
     maxPatternLength: 32,
     minMatchCharLength: 1,
+    threshold: 0.2,
+    location: 0,
+    distance: 100,
     keys: [
         'displayString'
     ]
