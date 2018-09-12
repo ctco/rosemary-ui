9767e52e38139f3d206375cea14fe6cf2aaf361b
kaspars.stauzhs01 <kaspars.stauzhs@ctco.lv>
2018-09-12 16:06:25 +0300

    More switch examples

--- src/Switch/Switch.stories.js
+++ src/Switch/Switch.stories.js
@@ -8,4 +8,16 @@ storiesOf('Switch', module)
         <Switch onChange={value => console.log(value)} checked title="This is Title" />
     ))
     .add('Disabled', () => <Switch title="title title" disabled />)
-    .add('Uncontrolled Switch', () => <Switch />);
+    .add('Uncontrolled Switch', () => <Switch />)
+    .add('All in one', () => <div style={{margin: "10px"}}>
+        <Switch title="This is title"/>
+        <br />
+        <br />
+        <Switch title="This is title" value true/>
+        <br />
+        <br />
+        <Switch title="This is title" disabled/>
+        <br />
+        <br />
+        <Switch title="This is title" value={true} disabled/>
+    </div>);
